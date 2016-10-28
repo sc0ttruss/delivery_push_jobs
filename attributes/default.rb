dea9ce2adff3976e0765342287cd1871a27e174a
sc0ttruss <scott.russell@inetmedia.co.uk>
2016-10-28 13:07:57 +0300

    changed url from delivery to automate

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,9 +10,9 @@ default['push_jobs']['base_package_url'] = 'file:///mnt/share/chef'
 # default['push_jobs']['packages']['push-jobs-client'] ='push-jobs-client-1.3.4-1.el7.x86_64.rpm'
 default['push_jobs']['packages']['push-jobs-client'] ='push-jobs-client-2.1.1-1.el7.x86_64.rpm'
 # default['push_jobs']['packages']['runit'] ='runit-2.1.2-3.el7.centos.x86_64.rpm'
-# Delivery and SUpermarket URL's to retrieve their certificates
+# automate and SUpermarket URL's to retrieve their certificates
 default['push_jobs']['url']['chef'] = 'chef.myorg.chefdemo.net'
-default['push_jobs']['url']['delivery'] = 'delivery.myorg.chefdemo.net'
+default['push_jobs']['url']['delivery'] = 'automate.myorg.chefdemo.net'
 default['push_jobs']['url']['supermarket'] = 'supermarket.myorg.chefdemo.net'
 
 # assume this node has alredy been bootstrapped
